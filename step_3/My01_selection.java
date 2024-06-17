cd6b6ed04e94ab53d0e7707fd12743868de59db2
Akash Sharma <soulakash20002@gmail.com>
2024-06-17 17:40:21 +0530

    Sorting

---
+++ step_3/My01_selection.java
@@ -0,0 +1,33 @@
+public class My01_selection {
+    // print method
+    static void printArray(int[] arr) {
+        System.out.print("Array: [ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]");
+    }
+
+    // selection sort
+    static void selection(int[] arr) {
+        for (int i = 0; i < arr.length - 1; i++) {
+            int min = i;
+            for (int j = i; j < arr.length; j++) {
+                if (arr[j] < arr[min]) {
+                    min = j;
+                }
+            }
+
+            int temp = arr[i];
+            arr[i] = arr[min];
+            arr[min] = temp;
+        }
+
+        printArray(arr);
+    }
+
+    public static void main(String[] args) {
+        int[] arr = {2, 4, 1, 6, 3, 5};
+        selection(arr);
+    }
+}
