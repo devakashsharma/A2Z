f5c6ab838289912b4a059275a367f1f3e2404f70
Akash Sharma <soulakash20002@gmail.com>
2024-06-19 20:32:47 +0530

    Remove Duplicate

--- step3_array/easy/My04_removeDuplicate.java
+++ step3_array/easy/My04_removeDuplicate.java
@@ -1,7 +1,40 @@
+import java.util.HashSet;
+
 public class My04_removeDuplicate {
+    // print method
+    static void printArray(int[] arr) {
+        System.out.print("Array: [ ");
+        for (int i = 0; i < arr.length; i++) {
+            System.out.print(arr[i] + " ");
+        }
+        System.out.print("]\n");
+    }
+
     // Brute Force
+    // TC - O(N), SC - O(N)
     static void removeBrute(int[] arr) {
-        int n = arr.length;
-        
+        HashSet<Integer> hSet = new HashSet<>();
+        for (int i = 0; i < arr.length; i++) hSet.add(arr[i]);
+        System.out.println(hSet);
+        System.out.println("Total unique element is: " + hSet.size());
+    }
+
+    // Optimal Force
+    // TC - O(N), SC - O(N)
+    static int optimalRemove(int[] arr) {
+        int num = 0;
+        for (int i = 1; i < arr.length; i++) {
+            if (arr[i] != arr[num]) {
+                arr[++num] = arr[i];
+            }
+        }
+
+        return num + 1;
+    }
+
+    public static void main(String[] args) {
+        // int[] arr = { 4, 6, 2, 2, 6, 8, 1, 4};
+        int[] arr = { 1, 1, 2};
+        removeBrute(arr);
     }
 }
