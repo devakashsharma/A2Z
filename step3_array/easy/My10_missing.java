73365cf1c19969d52c516a5fc2e6241b5fd99f85
Akash Sharma <soulakash20002@gmail.com>
2024-06-20 21:24:14 +0530

    Optimal Missing

--- step3_array/easy/My10_missing.java
+++ step3_array/easy/My10_missing.java
@@ -13,8 +13,25 @@ public class My10_missing {
         System.out.println("Missing Number: " + missing);
     }
 
+    // Optimal
+    static void optimalMissing(int[] arr) {
+        int n = arr.length - 1;
+        int xor1 = 0; 
+        int xor2 = 0;
+
+        for (int i = 0; i < n; i++) {
+            xor1 = xor1 ^ (i + 1);
+            xor2 = xor2 ^ arr[i];
+        }
+
+        xor2 ^= 5;
+
+        System.out.println(xor1 ^ xor2);
+    }
+
     public static void main(String[] args) {
         int[] arr = { 1, 2, 3, 4, 6 };
         bruteMissing(arr);
+        optimalMissing(arr);
     }
 }
