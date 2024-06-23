2b25c23209355f5a1059f4982b3bdef783dad1a8
Akash Sharma <soulakash20002@gmail.com>
2024-06-23 16:15:27 +0530

    Dutch National Flag Algorithm

--- step3_array/medium/My02_sort.java
+++ step3_array/medium/My02_sort.java
@@ -29,50 +29,53 @@ public class My02_sort {
     static void betterSort(ArrayList<Integer> arr) {
         int zero = 0, one = 1;
         for (int i = 0; i < arr.size(); i++) {
-            if (arr.get(i) == 0) zero++;
-            else if (arr.get(i) == 1) one++;
+            if (arr.get(i) == 0)
+                zero++;
+            else if (arr.get(i) == 1)
+                one++;
         }
-        
+
         for (int i = 0; i < zero; i++) {
             arr.set(i, 0);
         }
-        
+
         for (int i = zero; i < (zero + one); i++) {
             arr.set(i, 1);
         }
-        
+
         for (int i = (zero + one); i < arr.size(); i++) {
             arr.set(i, 2);
         }
-        
+
         System.out.println(arr);
     }
-
 
     // Better Approach - In Arr
     // TC - O(N), SC - O(1)
     static void betterSortInArr(int[] arr) {
         int zero = 0, one = 0, n = arr.length;
         for (int i = 0; i < arr.length; i++) {
-            if (arr[i] == 0) zero++;
-            else if (arr[i] == 1) one++;
+            if (arr[i] == 0)
+                zero++;
+            else if (arr[i] == 1)
+                one++;
         }
-        
+
         for (int i = 0; i < zero; i++) {
             arr[i] = 0;
         }
-        
+
         for (int i = zero; i < (zero + one); i++) {
             arr[i] = 1;
         }
-        
+
         for (int i = (zero + one); i < n; i++) {
             arr[i] = 2;
         }
-        
+
         System.out.println(arr);
     }
-    
+
     // optimal - Dutch National Flag Algorithm
     // TC - O(N), SC - O(1)
     static void optimalSort(int[] arr) {
@@ -84,7 +87,8 @@ public class My02_sort {
                 arr[mid] = temp;
                 low++;
                 mid++;
-            } else if (arr[mid] == 1) mid++;
+            } else if (arr[mid] == 1)
+                mid++;
             else {
                 int temp = arr[high];
                 arr[high] = arr[mid];
@@ -99,13 +103,14 @@ public class My02_sort {
     public static void main(String[] args) {
         int[] arr = { 0, 1, 2, 0, 0, 2 };
         sortNums(arr);
+        
         ArrayList<Integer> arrList = new ArrayList<>();
         for (int i = 0; i < arr.length; i++) {
             arrList.add(arr[i]);
         }
         betterSort(arrList);
 
-        int[] arr2 = {2,0,2,1,1,0};
+        int[] arr2 = { 2, 0, 2, 1, 1, 0 };
         optimalSort(arr2);
     }
 }
